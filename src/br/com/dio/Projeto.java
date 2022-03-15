594cfc3d4a7f2e6734fd4ab8cb5f3d97f6d4546e
wende <wenderrferreira89@gmail.com>
2022-03-14 22:37:57 -0300

    Primeiro commit

---
+++ src/br/com/dio/Projeto.java
@@ -0,0 +1,29 @@
+
+package br.com.dio;
+
+import br.com.dio.model.Cachorro;
+
+public class Projeto {
+
+	private static final String Cachorro = null;
+
+	public static void main(String[] args) {
+	
+	Cachorro cachorro = new Cachorro ();
+	Livros Livros = new Livros();
+	
+	System.out.println(Cachorro);
+	System.out.println(Livros);
+	   
+	    
+	    	/*int a = 2;
+		int b = 3;
+		System.out.println("Olá Mundo" + (a+b));*/
+
+	}
+}
+
+class Livros {
+	private String cor;
+	private String npag;
+}
